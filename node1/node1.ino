d67b421d56f92b8d8b55d01a0430f70dab7562f0
Ramon Henrique <rhpr@cin.ufpe.br>
2017-11-09 01:47:56 -0300

    Read alcohol sensor in nodeMcu rather than KL43Z

--- node1/node1.ino
+++ node1/node1.ino
@@ -6,7 +6,7 @@
 #define   MESH_PORT       5555
 
 #define VELMAX 115
-#define ALCOOLMAX 350 
+#define ALCOOLMAX 350
 #define OPCODE_BEBADO 0x40
 #define OPCODE_VEL 0x20
 
@@ -25,64 +25,67 @@ void receivedCallback( uint32_t from, String &msg ) {
 
   // verifica se recebeu payload via wifi.
   // se recebeu, envia via  bluetooth o pacote recebido
-    char btbuffer[13];
-    
-    msg.toCharArray(btbuffer,13);
-    btbuffer[12] = '\n';
-    swSer.write((char*) btbuffer,13);
-   
-   
+  char btbuffer[13];
+
+  msg.toCharArray(btbuffer, 13);
+  btbuffer[12] = '\n';
+  swSer.write((char*) btbuffer, 13);
+
+
 }
 
 void newConnectionCallback( bool adopt ) {
-  
+
 }
 
 void send_payload(byte opcode, int flagSelect)
 {
   swSer.write("ENCODE\n");
 
-  
-  if(flagSelect == 0)
+
+  if (flagSelect == 0)
     flag_alc = true;
   else
     flag_vel = true;
-  
-  while(swSer.available())
+
+  while (swSer.available())
   {
     byte data_received[12];
     String package;
-    for(int i = 0; i < 12;i++)
+    for (int i = 0; i < 12; i++)
     {
-        data_received[i] = swSer.read();
-        if(i == 11)
-        {
-          
-          if(flagSelect == 0)
-            flag_alc = false;
-          else
-            flag_vel = false;
-        }
+      data_received[i] = swSer.read();
+      if (i == 11)
+      {
+
+        if (flagSelect == 0)
+          flag_alc = false;
+        else
+          flag_vel = false;
+      }
     }
-   
+
     //Opcode representando condutor bebado
-    
+
     data_received[0] = data_received[0] | (byte) opcode;
-    
+
     //broadcast data via wifi
-    
+
     package = String((char*) data_received);
     Serial.println(package);
     mesh.sendBroadcast(package);
-    
+
   }
 }
 
 void setup() {
- Serial.begin(115200);
- swSer.begin(9600); /* Define baud rate for serial communication */
- 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  Serial.begin(115200);
+  swSer.begin(9600); /* Define baud rate for serial communication */
+
+  // pin to read mq3
+  pinMode(A0, INPUT);
+
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
@@ -90,46 +93,51 @@ void setup() {
   mesh.setNewConnectionCallback( &newConnectionCallback );
 }
 
-String s = "0123456789123";
+
 void loop() {
   mesh.update();
   delay(1000);
 
-  Serial.println(s);
-  if (Serial.available())
-  {
-    String data = Serial.readString();
-    String op = data.substring(0,4);
-    String strValue = data.substring(4);
-    int value = strValue.toInt();
-//    Serial.println(op);
-//    Serial.println("data: " +/ data);
-    if (op.equals("VEL-"))
-    {
-      velocidade = value;
-      s="velocidade";
-    }
-    if (op.equals("ALC-"))
-    {
-      sensorAlcool = value;
-      s="";
-      s=strValue;
-//      Serial.println(value);
-    }
-    
-  }
-//Le sensor de alcool;
-//Le sensor de velocidade
-  if(sensorAlcool > ALCOOLMAX || flag_alc)
-    //individuo bebado, monta payload
-    send_payload(OPCODE_BEBADO,0);
+//  Serial.println(s);
+//  if (Serial.available())
+//  {
+//    String data = Serial.readString();
+//    String op = data.substring(0, 4);
+//    String strValue = data.substring(4);
+//    int value = strValue.toInt();
+//    //    Serial.println(op);
+//    //    Serial.println("data: " +/ data);
+//    if (op.equals("VEL-"))
+//    {
+//      velocidade = value;
+//      s = "velocidade";
+//    }
+//    if (op.equals("ALC-"))
+//    {
+//      sensorAlcool = value;
+//      s = "";
+//      s = strValue;
+//      //      Serial.println(value);
+//    }
+//
+//  }
+
+   
+  //Le sensor de alcool;
+  sensorAlcool = analogRead(A0);
   
+  //Le sensor de velocidade
+  
+  if (sensorAlcool > ALCOOLMAX || flag_alc)
+    //individuo bebado, monta payload
+    send_payload(OPCODE_BEBADO, 0);
+
   else if (velocidade > VELMAX || flag_vel)
     //individuo em alta velocidade, monta payload
     send_payload(OPCODE_VEL, 1);
 
 
-  
+
 }
 
 
